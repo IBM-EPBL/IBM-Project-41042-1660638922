e92ec2c60f66ce1143b2351d64afc85464b3b263
gabrielfernando-j <94844696+gabrielfernando-j@users.noreply.github.com>
2022-11-08 15:58:55 +0530

    Create assignment3-trafficlight.py

---
+++ assessment/Lohith Aksha/assignment3-trafficlight.py
@@ -0,0 +1,18 @@
+from gpiozero import Button, TrafficLights, Buzzer
+import time
+
+buzz = Buzzer(15)
+b = Button(21)
+light = TrafficLights(25,8,7)
+
+while True:
+  b.wait_for_press()
+  buzz.on()
+  light.green.on()
+  time.sleep(1)
+  light.amber.on()
+  time.sleep(1)
+  light.red.on()
+  time.sleep(1)
+  light.off()
+  buzz.off()
