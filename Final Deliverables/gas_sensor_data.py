fb68d61f937cbd8e08cbd6b74a14c2ef9befef56
gabrielfernando-j <94844696+gabrielfernando-j@users.noreply.github.com>
2022-11-18 22:20:29 +0530

    Create gas_sensor_data.py

---
+++ Final Deliverables/gas_sensor_data.py
@@ -0,0 +1,30 @@
+import wiotp.sdk.device
+import time
+import random
+myConfig = { 
+    "identity": {
+        "orgId": "4192s3",
+        "typeId": "test",
+        "deviceId":"1234"
+    },
+    "auth": {
+        "token": "zUd@8CzC1U&caXn3Oj"
+    }
+}
+
+def myCommandCallback(cmd):
+    print("Message received from IBM IoT Platform: %s" % cmd.data['command'])
+    m=cmd.data['command']
+
+client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
+client.connect()
+
+while True:
+    gas = random.randint(100,720)
+    hum=random.randint(0,100)
+    myData={'gas_level':gas}
+    client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=0, onPublish=None)
+    print("Published data Successfully: %s", myData)
+    client.commandCallback = myCommandCallback
+    time.sleep(2)
+client.disconnect()
